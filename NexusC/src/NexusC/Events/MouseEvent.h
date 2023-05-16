c3f31ba7c4aa52f4495edb822bdee8f863d5f02b
AqourSoro <971755995@qq.com>
2023-05-16 15:08:52 +0800

    Update MouseEvent.h

--- NexusC/src/NexusC/Events/MouseEvent.h
+++ NexusC/src/NexusC/Events/MouseEvent.h
@@ -5,6 +5,7 @@
 #include <sstream>
 
 namespace NexusC
+
 {
 	class MouseMovedEvent : public Event
 	{
@@ -31,7 +32,26 @@ namespace NexusC
 
 	class MouseScrolledEvent : public Event
 	{
+	public:
+		MouseScrolledEvent(const float xOffset, const float yOffset)
+			:m_XOffset(xOffset), m_YOffset(yOffset) {}
 
+		float GetXOffset() const { return m_XOffset;  }
+		float GetYOffset() const { return m_YOffset;  }
+
+		std::string ToString() const override
+		{
+			std::stringstream ss;
+			ss << "MouseScrolledEvent: " << GetXOffset() << ", " << GetYOffset();
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(MouseScrolled)
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+
+
+	private:
+		float m_XOffset, m_YOffset;
 	};
 
 }
